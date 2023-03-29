1b85a0a9b54b90ea1efb9e7cb11763a2ca51dedb
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2023-03-29 09:29:48 -0300

    Adicionando arquivos sobre operadores

---
+++ entradas.py
@@ -0,0 +1,13 @@
+#Fazendo input de uma String
+nome = input()
+
+# Fazendo input de um número float
+preco = float(input())
+
+# Fazendo o input de um número inteiro
+idade = int(input())
+
+
+# Fazendo input de um boolean
+aprovado = bool(input())
+
---
+++ saidas.py
@@ -0,0 +1,9 @@
+soma = 2 + 2
+
+print("Hello World")
+
+print(2)
+
+print(soma)
+
+print(type(soma))
