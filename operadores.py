a5aa95d3ecd48589ce18ae890c3f60c465482187
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2023-03-29 09:39:37 -0300

    Vantagens do for

---
+++ operadores.py
@@ -0,0 +1,8 @@
+num1 = int(input("Por favor insira um número: "))
+num2 = int(input("Por favor insira outro número: "))
+
+total = num1 + num2
+
+print(total)
+print("----------------")
+print(num1 + num2)
